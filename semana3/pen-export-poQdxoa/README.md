a803c37c72055901a54547c58b7945bc5dabeb6f
Lucas <lucasrodriguezy@gmail.com>
2023-07-10 16:08:16 -0300

    Changing pages

---
+++ semana3/pen-export-poQdxoa/README.md
@@ -0,0 +1,4 @@
+# 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/lucas-rodrigues-the-builder/pen/poQdxoa](https://codepen.io/lucas-rodrigues-the-builder/pen/poQdxoa).
+
---
+++ semana3/pen-export-poQdxoa/dist/script.js
@@ -0,0 +1,13 @@
+function Advertise(){
+  
+  alert("Congratulations, now your computer have virus!!");
+  
+}
+function Confirm(boolean choice){
+  boolean choice;
+  if(choice == true){
+    var ok = ("you pressed OK, thank you!")
+  }else{
+    var not = ("Are you sure you want to cancel?")
+  }
+  }
