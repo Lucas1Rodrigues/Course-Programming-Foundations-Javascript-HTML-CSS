a3c0c24ab701e9caad3a22415fd9928f88877a13
Lucas <lucasrodriguezy@gmail.com>
2023-07-20 12:33:43 -0300

    Exercice 1

---
+++ semana3/change-color/README.md
@@ -0,0 +1,4 @@
+# change color
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/lucas-rodrigues-the-builder/pen/zYMpNZr](https://codepen.io/lucas-rodrigues-the-builder/pen/zYMpNZr).
+
---
+++ semana3/change-color/dist/script.js
@@ -0,0 +1,15 @@
+function changeColor(){
+  var divElement1 = document.getElementById("div1");
+  var divElement2 = document.getElementById("div2");
+  
+  divElement1.className = "fuchsiaback";
+  divElement2.className = "blueback";
+}
+
+function changeText(){
+  var divElement1 = document.getElementById("div1");
+  var divElement2 = document.getElementById("div2");
+  
+  divElement1.innerHTML = "zweite";
+  divElement2.innerHTML = "heimpf";
+}
